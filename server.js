d59690493ef5d200271fb8afdcd15136d7de06fa
zenglenn42 <gsnospampls@pobox.com>
2019-10-29 11:12:02 -0500

    add route for sending survey json to the client

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require("express");
+var survey = require("./app/data/survey.js")
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+console.log(survey);
 
 app.get("/", (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end("<h1>Welcome to Friend Finder</h1>");
 })
+
+app.get("/survey.json", (req, res) => {
+    res.json(survey);
+});
 
 app.listen(PORT, () => {
     console.log(`Friend Finder server listening on port ${PORT}`);
