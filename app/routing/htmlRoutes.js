c48d2a24167339e259992e05b565c6a74b13779e
zenglenn42 <gsnospampls@pobox.com>
2019-11-07 09:27:00 -0600

    move frontend js to its own file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,10 @@ module.exports = function(app) {
     app.get("/style.css", (req, res) => {
         res.sendFile(path.join(__dirname, "/../public/style.css"));
     });
+
+    app.get("/controller.js", (req, res) => {
+        res.sendFile(path.join(__dirname, "/../public/controller.js"));
+    });
     
     app.get("/survey.html", (req, res) => {
         res.send(surveyHtml);
